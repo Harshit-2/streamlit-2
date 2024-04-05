2c406a220559da89fc1552c6ae0fc859da10e543
Harshit <102576901+Harshit-2@users.noreply.github.com>
2024-04-05 11:40:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+import requests
 
+API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Can you please let us know more details about your ",
+})
